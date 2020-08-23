de05cf0d75d4097c0580fc13c11bb3b93624390e
Marco Corrias <marco.corrias2@studio.unibo.it>
2020-08-23 11:55:46 +0200

    Removed useless argument inside the swap function; minor graphical changes

--- functions.py
+++ functions.py
@@ -27,9 +27,9 @@ def travel(N):
     Returns:
         city: array containing the cities coordinates.
     '''    
-    city = np.zeros((2,N))
+    city = np.zeros((2, N))
     for i in range(N):
-        for j in (0,1):
+        for j in (0, 1):
             city[j][i] = rand.rand()
             
     return city
@@ -46,14 +46,14 @@ def length(N, city, citynum):
     Returns:
         leng: length of the travel.
     '''
-    leng=0.
+    leng = 0.
     for i in range(N):
-        dum=0.;
-        for icoo in (0,1): #coo=coordinate, loop for each component
-            iloc1=int(citynum[i])
-            iloc2=int(citynum[i-1])
-            dum+=(city[icoo][iloc1]-city[icoo][iloc2])**2
-        leng+=np.sqrt(dum)
+        dum = 0.;
+        for icoo in (0, 1): #coo=coordinate, loop for each component
+            iloc1 = int(citynum[i])
+            iloc2 = int(citynum[i-1])
+            dum += (city[icoo][iloc1]-city[icoo][iloc2])**2
+        leng += np.sqrt(dum)
         
     return leng
 
@@ -67,10 +67,10 @@ def Tem_length(T, T_min, alpha):
     Returns:
         The number of temperatures considered.
     '''
-    count=0
+    count = 0
     while T > T_min:
-        count+=1
-        T=T*alpha
+        count += 1
+        T = T*alpha
         
     return count
 
@@ -84,9 +84,9 @@ def get_path(N, city, citynum):
         citynum: list containing the number associated to each city,
                  and their visiting order (i.e. the list index number).
     '''
-    path=np.zeros((2,N+1))
+    path = np.zeros((2, N+1))
     for k in range(N):
-        for i in (0,1):
+        for i in (0, 1):
             path[i][k]=city[i][citynum[k]]
     path[0][N]=city[0][citynum[0]]
     path[1][N]=city[1][citynum[0]]  
@@ -97,7 +97,7 @@ def get_path(N, city, citynum):
 #------------------------------------------------------------------------------|
 
     
-def breverse(x,y, citynum):
+def breverse(x, y, citynum):
     '''
     Executes a block reverse move.
     Parameters:
@@ -107,15 +107,15 @@ def breverse(x,y, citynum):
     Returns:
         citynum: updated citynum.
     '''
-    j=max(x,y)
-    i=min(x,y)
-    dummy=citynum[i:j+1]
-    citynum[i:j+1]=dummy[::-1] 
+    j = max(x,y)
+    i = min(x,y)
+    dummy = citynum[i:j+1]
+    citynum[i:j+1] = dummy[::-1] 
     
     return citynum
     
 
-def swap(i,j,N, citynum):
+def swap(i, j, citynum):
     '''
     Executes a swap move.
     Parameters:
@@ -126,14 +126,14 @@ def swap(i,j,N, citynum):
     Returns:
         citynum: updated citynum.
     '''
-    save=citynum[i]
-    citynum[i]=citynum[j]
-    citynum[j]=save
+    save = citynum[i]
+    citynum[i] = citynum[j]
+    citynum[j] = save
     
     return citynum
 
 
-def prunegraft(x,y,z, citynum):
+def prunegraft(x , y, z, citynum):
     '''
     Executes a prune and graft move.
     Parameters:
@@ -144,25 +144,25 @@ def prunegraft(x,y,z, citynum):
         citynum: updated citynum.
 
     '''
-    i=min(x,y)
-    j=max(x,y)
-    k=z
-    if k>i and k<j:
-        a=citynum[0:i]
-        b=citynum[j:N]
-        dummy=b+a
-        dummy=dummy[::-1]
+    i = min(x, y)
+    j = max(x, y)
+    k = z
+    if k > i and k < j:
+        a = citynum[0:i]
+        b = citynum[j:N]
+        dummy = b+a
+        dummy = dummy[::-1]
         for m in range(len(dummy)):
-            citynum.insert(k+m,dummy[m])
+            citynum.insert(k+m, dummy[m])
         del citynum[j+len(dummy):N+len(dummy)]
         del citynum[0:i]
-    if k<=i:
-        dummy=citynum[i:j]
+    if k <= i:
+        dummy = citynum[i:j]
         for m in range(len(dummy)):
-            citynum.insert(k+m,dummy[m])
+            citynum.insert(k+m, dummy[m])
         del citynum[i+len(dummy):j+len(dummy)]
-    if k>=j:
-        dummy=citynum[i:j]
+    if k >= j:
+        dummy = citynum[i:j]
         for m in range(len(dummy)):
             citynum.insert(k+m,dummy[m])
         del citynum[i:j] 
@@ -195,27 +195,26 @@ def anneal_BRev_distance(N, alpha, T, city, T_min, nstep, citynum):
     accelist = []
     old=length(N, city, citynum)
     while T > T_min:
-        rand.seed(); acce=0; ii=0
+        rand.seed(); acce = 0; ii = 0
         while ii <= nstep:
-            ii+=1
-            x=int(N*rand.rand())
-            y=int(N*rand.rand())
-            while x>(N-1) and y>(N-1) : # In order to avoid IndexError
-                x=int(N*rand.rand())
-                y=int(N*rand.rand())
-            citynum=breverse(x,y, citynum)
-            new=length(N, city, citynum) 
+            ii += 1
+            x = int(N*rand.rand())
+            y = int(N*rand.rand())
+            while x > (N-1) and y > (N-1) : # In order to avoid IndexError
+                x = int(N*rand.rand())
+                y = int(N*rand.rand())
+            citynum = breverse(x, y, citynum)
+            new = length(N, city, citynum) 
             
             if np.exp(-(new-old)/T) >= 1:
-                old=new
-                acce+=1
-                new=length(N, city, citynum)
+                old = new
+                acce += 1
+                new = length(N, city, citynum)
             else:
-                citynum=breverse(y,x, citynum) 
+                citynum=breverse(y, x, citynum) 
                 
         Tem.append(T)
         accelist.append(100*float(acce)/nstep)
-        #if acce==0: break
         T = T*alpha
         
     return citynum, accelist, Tem
@@ -243,27 +242,26 @@ def anneal_BRev_Metropolis(N, alpha, T, city, T_min, nstep, citynum):
     accelist = []
     old=length(N, city, citynum)
     while T > T_min:
-        rand.seed(); acce=0; ii=0
+        rand.seed(); acce = 0; ii = 0
         while ii <= nstep:
-            ii+=1
-            x=int(N*rand.rand())
-            y=int(N*rand.rand())
-            while x>(N-1) and y>(N-1) : # In order to avoid IndexError
-                x=int(N*rand.rand())
-                y=int(N*rand.rand())
-            citynum=breverse(x,y, citynum)
-            new=length(N, city, citynum)
+            ii += 1
+            x = int(N*rand.rand())
+            y = int(N*rand.rand())
+            while x > (N-1) and y > (N-1) : # In order to avoid IndexError
+                x = int(N*rand.rand())
+                y = int(N*rand.rand())
+            citynum = breverse(x, y, citynum)
+            new = length(N, city, citynum)
             
             if np.exp(-(new-old)/T) > rand.rand():
-                old=new
-                acce+=1
-                new=length(N,city, citynum)
+                old = new
+                acce += 1
+                new = length(N, city, citynum)
             else:
-                citynum=breverse(x,y, citynum)
+                citynum = breverse(x, y, citynum)
                 
         Tem.append(T)
         accelist.append(100*float(acce)/nstep)
-        #if acce==0: break
         T = T*alpha  
         
     return citynum, accelist, Tem
@@ -287,32 +285,30 @@ def anneal_swap_Metropolis(N, T, alpha, city, T_min, nstep, citynum):
         accelist: list containing the acceptance rate for each temperature.   
         Tem: list containing the temperatures.
     '''
-    Tem=[]
-    accelist=[]
+    Tem = []
+    accelist = []
     old=length(N, city, citynum)
     while T > T_min:
-        rand.seed(); acce=0; ii=0
+        rand.seed(); acce = 0; ii = 0
         while ii <= nstep:
-            ii+=1
-            ir=int(N*rand.rand())
-            ir2=int(N*rand.rand())
-            while ir>(N-1) and ir2>(N-1) : # In order to avoid IndexError
-                ir=int(N*rand.rand())
-                ir2=int(N*rand.rand())
-            citynum = swap(ir,ir2,N, citynum)
-            new=length(N, city, citynum)
+            ii += 1
+            ir = int(N*rand.rand())
+            ir2 = int(N*rand.rand())
+            while ir > (N-1) and ir2 > (N-1) : # In order to avoid IndexError
+                ir = int(N*rand.rand())
+                ir2 = int(N*rand.rand())
+            citynum = swap(ir, ir2, citynum)
+            new = length(N, city, citynum)
             
             if np.exp(-(new-old)/T) > rand.rand():
-                old=new
-                acce+=1
-                new=length(N, city, citynum)
+                old = new
+                acce += 1
+                new = length(N, city, citynum)
             else:
-                citynum = swap(ir2,ir,N, citynum)   
-                print('w=',np.exp(-(new-old)/T))
+                citynum = swap(ir2,ir, citynum)   
                 
         Tem.append(T)
         accelist.append(100*float(acce)/nstep)
-        #if acce==0: break
         T = T*alpha
         
     return citynum, accelist, Tem
@@ -336,31 +332,30 @@ def anneal_swap_distance(N, alpha, T, city, T_min, nstep, citynum):
         accelist: list containing the acceptance rate for each temperature.   
         Tem: list containing the temperatures.
     '''
-    Tem=[]
-    accelist=[]
-    old=length(N,city, citynum)
+    Tem = []
+    accelist = []
+    old = length(N, city, citynum)
     while T > T_min:
         rand.seed(); acce=0; ii=0
         while ii <= nstep:
-            ii+=1
-            ir=int(N*rand.rand())
-            ir2=int(N*rand.rand())
-            while ir>(N-1) and ir2>(N-1) : # In order not to get and IndexError
-                ir=int(N*rand.rand())
-                ir2=int(N*rand.rand())
-            citynum = swap(ir,ir2,N, citynum)
-            new=length(N,city, citynum)
+            ii += 1
+            ir = int(N*rand.rand())
+            ir2 = int(N*rand.rand())
+            while ir > (N-1) and ir2 > (N-1) : # In order not to get and IndexError
+                ir = int(N*rand.rand())
+                ir2 = int(N*rand.rand())
+            citynum = swap(ir, ir2, citynum)
+            new = length(N, city, citynum)
             
             if np.exp(-(new-old)/T) >= 1:
-                old=new
-                acce+=1
-                new=length(N, city, citynum)
+                old = new
+                acce += 1
+                new = length(N, city, citynum)
             else:
-                citynum = swap(ir2,ir,N, citynum)
+                citynum = swap(ir2, ir, citynum)
         
         Tem.append(T)
         accelist.append(float(acce)/nstep)
-        #if acce==0: break
         T = T*alpha
         
     return citynum, accelist, Tem
@@ -388,32 +383,31 @@ def anneal_PG_Metropolis(N, alpha, T, city, T_min, nstep, citynum):
     accelist = []
     old=length(N, city, citynum)
     while T > T_min:
-        rand.seed(); acce=0; ii=0
+        rand.seed(); acce = 0; ii = 0
         while ii <= nstep:
-            ii+=1
-            save=[]    #needed in case of move rejection
+            ii += 1
+            save = []    #needed in case of move rejection
             for m in range(N):
                 save.append(citynum[m])
-            x=int(N*rand.rand())
-            y=int(N*rand.rand())
-            z=int(N*rand.rand())
-            while x>(N-1) and y>(N-1) and z>(N-1) : # In order to avoid IndexError
-                x=int(N*rand.rand())
-                y=int(N*rand.rand())
-                z=int(N*rand.rand())
-            citynum = prunegraft(x,y,z, citynum) 
-            new=length(N, city, citynum)
+            x = int(N*rand.rand())
+            y = int(N*rand.rand())
+            z = int(N*rand.rand())
+            while x > (N-1) and y > (N-1) and z > (N-1) : # In order to avoid IndexError
+                x = int(N*rand.rand())
+                y = int(N*rand.rand())
+                z = int(N*rand.rand())
+            citynum = prunegraft(x, y, z, citynum) 
+            new = length(N, city, citynum)
             
             if np.exp(-(new-old)/T) > rand.rand():
-                old=new
-                acce+=1
-                new=length(N, city, citynum)
+                old = new
+                acce += 1
+                new = length(N, city, citynum)
             else:
-                citynum=save
+                citynum = save
         
         Tem.append(T)
         accelist.append(100*float(acce)/nstep)
-        #if acce==0: break
         T = T*alpha
         
     return citynum, accelist, Tem
@@ -441,32 +435,31 @@ def anneal_PG_distance(N, alpha, T, city, T_min, nstep, citynum):
     accelist = []
     old=length(N, city, citynum)
     while T > T_min:
-        rand.seed(); acce=0; ii=0
+        rand.seed(); acce = 0; ii = 0
         while ii <= nstep:
-            ii+=1
-            save=[]
+            ii += 1
+            save = []
             for m in range(N):
                 save.append(citynum[m])
-            x=int(N*rand.rand())
-            y=int(N*rand.rand())
-            z=int(N*rand.rand())
-            while x>(N-1) and y>(N-1) and z>(N-1) : # In order to avoid IndexError
-                x=int(N*rand.rand())
-                y=int(N*rand.rand())
-                z=int(N*rand.rand())
-            citynum = prunegraft(x,y,z, citynum)
-            new=length(N, city, citynum)
+            x = int(N*rand.rand())
+            y = int(N*rand.rand())
+            z = int(N*rand.rand())
+            while x > (N-1) and y > (N-1) and z > (N-1) : # In order to avoid IndexError
+                x = int(N*rand.rand())
+                y = int(N*rand.rand())
+                z = int(N*rand.rand())
+            citynum = prunegraft(x, y, z, citynum)
+            new = length(N, city, citynum)
             
             if np.exp(-(new-old)/T) >= 1:
-                old=new
-                acce+=1
-                new=length(N, city, citynum)
+                old = new
+                acce += 1
+                new = length(N, city, citynum)
             else:
-                citynum=save
+                citynum = save
             
         Tem.append(T)
         accelist.append(100*float(acce)/nstep)
-        #if acce==0: break
         T = T*alpha  
         
     return citynum, accelist, Tem
